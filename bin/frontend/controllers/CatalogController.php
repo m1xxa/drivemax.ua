51e3c1e2e8e12a2947788718e1a804b31c8b8db7
m1xxa <m1sha2004@mail.ru>
2016-10-18 15:22:31 +0300

    small bug fix

--- bin/frontend/controllers/CatalogController.php
+++ bin/frontend/controllers/CatalogController.php
@@ -16,7 +16,7 @@ class CatalogController extends \yii\web\Controller
     public function actionView($category){
         $categoryId = Category::find()->where(['alias' => $category])->one();
         $model = Category::find()->where(['parent_id' => $categoryId->category_id])->all();
-        $products = Product::find()->join(['category'])->where(['alias' => $categoryId->alias])->all();
+        $products = Product::find()->joinWith(['category'])->where(['alias' => $categoryId->alias])->all();
         return $this->render('index', ['model' => $model, 'products' => $products, 'categoryid' => $categoryId]);
     }
 
